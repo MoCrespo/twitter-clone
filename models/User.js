661c1bff20096be92ea1f44bf92ad5a40311690c
MoCrespo <mcrespo495@gmail.com>
2022-01-12 16:35:23 +0200

    create the user model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  full_name: {
+    type: String,
+    required: true,
+  },
+  user_name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  cover: {
+    type: String,
+  },
+});
+
+module.exports = User = mongoose.model("user", userSchema);
